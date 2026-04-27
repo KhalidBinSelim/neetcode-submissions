ea22bd03843d70589db89161e5d6cb1e8dacaf0f
Khalid Bin Selim <131846762+KhalidBinSelim@users.noreply.github.com>
2026-04-27 23:31:27 +0600

    Add: sliding-window-maximum - submission-2

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> res(n - k + 1);
+        deque<int> dq;
+        int l = 0, r = 0;
+        while (r < n) {
+            while (!dq.empty() and nums[dq.back()] < nums[r]) dq.pop_back();
+            dq.push_back(r);
+            if (l > dq.front()) dq.pop_front();
+            if ((r + 1) >= k) {
+                res[l] = nums[dq.front()];
+                l++;
+            }
+            r++;
+        }
+        return res;
+    }
+};
