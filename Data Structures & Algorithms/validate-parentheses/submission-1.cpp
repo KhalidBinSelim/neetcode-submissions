cdd6c67b5210850ebbb99f91bfd49a363fe4ec93
Khalid Bin Selim <131846762+KhalidBinSelim@users.noreply.github.com>
2026-04-28 00:18:34 +0600

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char c : s) {
+            if (c == '(') st.push(')');
+            else if (c == '{') st.push('}');
+            else if (c == '[') st.push(']');
+            else {
+                if (st.empty() or c != st.top()) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
